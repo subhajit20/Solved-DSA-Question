600c13f59710b69172f99813b4875fe5104bb34e
Subhajit Ghosh <subhajitstd07@gmail.com>
2025-02-13 21:18:56 +0530

    Added string practices

--- learning_DSA/array/ReverseArray.js
+++ learning_DSA/array/ReverseArray.js
@@ -3,6 +3,8 @@ let array = [1,3,4,5,6];
 let start = 0;
 let end = array.length - 1;
 
+
+
 while(start < end){
     let temp = array[start];
     array[start] = array[end];
---
+++ learning_DSA/string/ReverseString.js
@@ -0,0 +1,12 @@
+let str = 'Subhajit';
+
+
+let reveredStr = '';
+for(let i = str.length - 1; i >= 0; i--){
+    console.log(str[i]);    
+    reveredStr += str[i];
+}
+
+
+console.log('Main string ---> ',str);
+console.log('Reversed string ---> ', reveredStr);
